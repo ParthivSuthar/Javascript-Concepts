23816d3683dd42ec634727bbfe9906ec3ef05a85
ParthivSuthar <143894333+ParthivSuthar@users.noreply.github.com>
2023-09-12 17:35:56 +0000

    Js - Data Types

---
+++ Js-Basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //Treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "Google"
+let age = "20"
+let isLoggedin = true
+
+//number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null
+// undefined
+// symbol => unique
+
+//object
+
+console.log(typeof age); //O : String
+console.log(typeof null); // O : Object
+console.log(typeof undefined); // O : undefined
+
+
