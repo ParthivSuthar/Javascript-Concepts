ece4a37d8d5217e80b4f19f2a3be3c81b036e58e
ParthivSuthar <143894333+ParthivSuthar@users.noreply.github.com>
2023-09-13 17:26:34 +0000

    operations

--- Js-Basics/datatype-conversion.js
+++ Js-Basics/datatype-conversion.js
@@ -1,25 +1,52 @@
 let score = 100
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN Not a Number
 // true=1; false=0;
 
 // Use String
-let score = 1
+// let score = 1
 
-let somenunber = String(score)
-console.log(typeof somenunber);
+// let somenunber = String(score)
+// console.log(typeof somenunber);
 
-// Using Boolean
-let score = "Hello"
-let booleanscore = Boolean(score)
-console.log(booleanscore);
+// // Using Boolean
+// let score = "Hello"
+// let booleanscore = Boolean(score)
+// console.log(booleanscore);
 
 // 1 => true; same as 0 => false
 // "" => false and "hello" => true
+
+
+// ***************** Operations *****************
+
+let number = 10
+let negValue = -number;
+
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " hi"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+let gambar = 500
+gambar++
+
+console.log(gambar);
