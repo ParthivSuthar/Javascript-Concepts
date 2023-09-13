28fa7fd034dbc1923e32726368d0479be5012024
ParthivSuthar <143894333+ParthivSuthar@users.noreply.github.com>
2023-09-13 16:36:47 +0000

    Datatype conversion Conffusion

---
+++ Js-Basics/datatype-conversion.js
@@ -0,0 +1,25 @@
+let score = 100
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN Not a Number
+// true=1; false=0;
+
+// Use String
+let score = 1
+
+let somenunber = String(score)
+console.log(typeof somenunber);
+
+// Using Boolean
+let score = "Hello"
+let booleanscore = Boolean(score)
+console.log(booleanscore);
+
+// 1 => true; same as 0 => false
+// "" => false and "hello" => true
