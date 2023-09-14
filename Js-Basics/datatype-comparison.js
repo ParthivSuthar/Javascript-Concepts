962efa1b7756b6158e5483e196db9767014d1ca9
ParthivSuthar <143894333+ParthivSuthar@users.noreply.github.com>
2023-09-14 16:32:59 +0000

    Comparison of Data types

---
+++ Js-Basics/datatype-comparison.js
@@ -0,0 +1,17 @@
+// console.log("2" > 3);  it automatically convert to number
+// console.log("2" <= 3);
+// console.log("2" >= 3);
+// console.log("2" == 3);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+console.log("3" === 5);  //called as Strictly check which check datatype also 😯
+
+// ❌ Don't follow this confusion ❌
+
