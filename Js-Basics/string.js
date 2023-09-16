db799b16660caa7e95d5cf1c8e2929bb7f226fa6
ParthivSuthar <143894333+ParthivSuthar@users.noreply.github.com>
2023-09-16 16:02:02 +0000

    String concept

---
+++ Js-Basics/string.js
@@ -0,0 +1,55 @@
+const name = "Paresh"
+const age = 56
+
+console.log(`Hello, my name is ${name} and my age is ${age}`);
+
+const foodName = new String('Paneer')
+
+// console.log(foodName)
+
+const cityName = new String('Mehsana')
+
+// console.log(cityName[4]);
+
+// console.log(cityName.__proto__);  // gives {} empty object
+
+const stateName = new String('Gujarat')
+// console.log(stateName.length);
+
+const placeName = new String('Spain')
+// console.log(placeName.toLowerCase());
+// console.log(placeName.toUpperCase());
+
+const godName = new String('Mahadev')
+const newgodName = godName.substring(0,5)
+
+// console.log(newgodName);
+
+const treeName = new String('Banyan')
+
+// console.log(treeName.charAt(2));
+
+// console.log(treeName.indexOf('y'));
+
+const fruitName = new String('Mango')
+const newfruitName = fruitName.slice(-2,6)
+
+console.log(newfruitName);
+
+const youtubeName = new String('Dockey')
+const newyoutubeName = "  Hello Baba   "
+
+// console.log(newyoutubeName.trim());
+
+
+const techName = new String('React')
+const newtechName = "angular23@gmail.com"
+
+// console.log(newtechName.replace('23', '-'));
+
+// console.log(newtechName.includes('23')); 
+
+const newfoodName = new String('Pav-Bhaji')
+// console.log(newfoodName.split('-'));
+
+
