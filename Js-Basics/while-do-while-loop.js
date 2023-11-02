b5bb4e3971a690f2b2e0eb9ade131238b18b19bf
ParthivSuthar <143894333+ParthivSuthar@users.noreply.github.com>
2023-11-02 15:32:34 +0000

    While and Do while Loop

---
+++ Js-Basics/while-do-while-loop.js
@@ -0,0 +1,39 @@
+// While Loop
+
+let i = 0
+while(i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i += 2
+}
+
+
+
+// While loop in array
+
+let myarr = ["Jio","Airtel","VI"]
+let arr = 0
+while(arr < myarr.length) {
+    console.log(`The value myarr is ${myarr[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// Do While Loop
+
+let run = 0
+
+do {
+    console.log(`Value is ${run}`);
+    run++
+}while(run <= 10)
+
+
+// If run == 12 🤔👇
+ 
+let run2 = 12
+
+do {
+    console.log(`Value is ${run2}`);
+    run++
+}while(run2 <= 10)
